101c50144325fc9167148ac3b0e55f0e715e35eb
PaulWzlff <paulwenzlaff@gmail.com>
2019-08-08 21:34:11 +0200

    Init Interface

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
 const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
+const shell = require('electron').shell
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 1000,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile('src/index.html')
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        win = null
+    })
 
- // for later use cases 
- var menu = Menu.buildFromTemplate([
-    {
-        label: 'Menu',
-        submenu:[
-            {label: 'Exit'}
-        ]
-    }
-])
+    // Top Menu
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                {
+                    label: 'Website',
+                    click(){
+                        shell.openExternal('http://securuscoin.org/')
+                    }
+                },
+                {type: 'separator'},
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit();
+                    }
+                }                
+            ]
+        }
+    ])
 
-Menu.setApplicationMenu(null);
+    //pick menu if Menu is wanted
+    Menu.setApplicationMenu(null);
 }
 
 // This method will be called when Electron has finished
@@ -44,17 +58,17 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
