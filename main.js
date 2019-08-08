578461a500068939a0254e41c4dc493d882e4692
PaulWzlff <paulwenzlaff@gmail.com>
2019-08-08 20:39:33 +0200

    Init GUI

---
+++ main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+
+ // for later use cases 
+ var menu = Menu.buildFromTemplate([
+    {
+        label: 'Menu',
+        submenu:[
+            {label: 'Exit'}
+        ]
+    }
+])
+
+Menu.setApplicationMenu(null);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
