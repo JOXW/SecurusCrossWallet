9e1ce676469b27f1403d3af56b18e713933b17b7
PaulWzlff <paulwenzlaff@gmail.com>
2019-08-10 11:51:58 +0200

    Splash Screen implemented

--- main.js
+++ main.js
@@ -2,70 +2,225 @@ const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 const shell = require('electron').shell
+const splash = require('@trodi/electron-splashscreen');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
+// function createWindow() {
+//     win = new BrowserWindow({
+//         width: 1400,
+//         height: 900,
+//         icon: 'src/assets/icon.ico',
+//     })
+
+//     win.loadFile('src/html/index.html')
+
+//     win.on('closed', () => {
+//         win = null
+//     })
+// }
+
 function createWindow() {
-    // Create the browser window.
-    win = new BrowserWindow({
+    const winOpts = {
+        title: "Securus Wallet",
+        icon: path.join(__dirname, 'src/assets/icon.ico'),
+        frame: true,
         width: 1400,
         height: 900,
-        icon: 'src/assets/icon.ico',
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+        show: false,
+        center: true,
+        autoHideMenuBar: false,
+        menuBarVisibility: false,
+        // webPreferences: {
+        //     nativeWindowOpen: true,
+        //     nodeIntegrationInWorker: true,
+        // },
+    };
 
-    // and load the index.html of the app.
-    win.loadFile('src/html/index.html')
+    win = splash.initSplashScreen({
+        windowOpts: winOpts,
+        templateUrl: path.join(__dirname, "src/html/splash.html"),
+        delay: 0,
+        minVisible: 5000,
+        splashScreenOpts: {
+            width: 1092,
+            height: 606,
+            transparent: true
+        },
+    });
 
-    // Emitted when the window is closed.
+    //load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/html/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // open devtools
+    //win.webContents.openDevTools();
+
+    // show windosw
+    // win.once('ready-to-show', () => {
+    //     //win.show();
+    //     win.setTitle("Securus Wallet");
+    //     if (platform !== 'darwin') {
+    //         tray.setToolTip(config.appSlogan);
+    //     }
+    // });
+
+    win.once('ready-to-show', () => {
+        //win.show();
+        win.setTitle("Securus Wallet");
+    });
+
     win.on('closed', () => {
         win = null
     })
 
-    // Top Menu
-    var menu = Menu.buildFromTemplate([{
-        label: 'Menu',
-        submenu: [{
-                label: 'Website',
-                click() {
-                    shell.openExternal('http://securuscoin.org/')
-                }
-            },
-            { type: 'separator' },
-            {
-                label: 'Exit',
-                click() {
-                    app.quit();
-                }
-            }
-        ]
-    }])
+    // win.on('close', (e) => {
+    //     if ((settings.get('tray_close') && !app.needToExit && platform !== 'darwin')) {
+    //         e.preventDefault();
+    //         win.hide();
+    //     } else if (app.prompExit) {
+    //         e.preventDefault();
+    //         if (app.prompShown) return;
+    //         let msg = 'Are you sure want to exit?';
+    //         app.prompShown = true;
+    //         dialog.showMessageBox({
+    //             type: 'question',
+    //             buttons: ['Yes', 'No'],
+    //             title: 'Exit Confirmation',
+    //             message: msg
+    //         }, function(response) {
+    //             app.prompShown = false;
+    //             if (response === 0) {
+    //                 app.prompExit = false;
+    //                 win.webContents.send('cleanup', 'Clean it up, Dad!');
+    //             } else {
+    //                 app.prompExit = true;
+    //                 app.needToExit = false;
+    //             }
+    //         });
+    //     }
+    // });
 
-    //pick menu if Menu is wanted
-    Menu.setApplicationMenu(null);
+    // if (platform !== 'darwin') {
+    //     let contextMenu = Menu.buildFromTemplate([
+    //         { label: 'Minimize to tray', click: () => { win.hide(); } },
+    //         {
+    //             label: 'Quit',
+    //             click: () => {
+    //                 app.needToExit = true;
+    //                 if (win) {
+    //                     win.close();
+    //                 } else {
+    //                     process.exit(0);
+    //                 }
+    //             }
+    //         }
+    //     ]);
+
+    // tray = new Tray(trayIcon);
+    // tray.setPressedImage(trayIconHide);
+    // tray.setTitle(config.appName);
+    // tray.setToolTip(config.appSlogan);
+    // tray.setContextMenu(contextMenu);
+
+
+    // tray.on('click', () => {
+    //     if (!win.isFocused() && win.isVisible()) {
+    //         win.focus();
+    //     } else if (settings.get('tray_minimize', false)) {
+    //         if (win.isVisible()) {
+    //             win.hide();
+    //         } else {
+    //             win.show();
+    //         }
+    //     } else {
+    //         if (win.isMinimized()) {
+    //             win.restore();
+    //             win.focus();
+    //         } else {
+    //             win.minimize();
+    //         }
+    //     }
+    // });
+
+    // win.on('show', () => {
+    //     tray.setHighlightMode('always');
+    //     tray.setImage(trayIcon);
+    //     contextMenu = Menu.buildFromTemplate([
+    //         { label: 'Minimize to tray', click: () => { win.hide(); } },
+    //         {
+    //             label: 'Quit',
+    //             click: () => {
+    //                 app.needToExit = true;
+    //                 win.close();
+    //             }
+    //         }
+    //     ]);
+    //     tray.setContextMenu(contextMenu);
+    //     tray.setToolTip(config.appSlogan);
+    // });
+
+    // win.on('hide', () => {
+    //     tray.setHighlightMode('never');
+    //     tray.setImage(trayIconHide);
+    //     if (platform === 'darwin') return;
+
+    //     contextMenu = Menu.buildFromTemplate([
+    //         { label: 'Restore', click: () => { win.show(); } },
+    //         {
+    //             label: 'Quit',
+    //             click: () => {
+    //                 app.needToExit = true;
+    //                 win.close();
+    //             }
+    //         }
+    //     ]);
+    //     tray.setContextMenu(contextMenu);
+    // });
+
+    // win.on('minimize', (event) => {
+    //     if (settings.get('tray_minimize') && platform !== 'darwin') {
+    //         event.preventDefault();
+    //         win.hide();
+    //     }
+    // });
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    win.setMenu(null);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
+
+// // misc handler
+// win.webContents.on('crashed', () => {
+//     // todo: prompt to restart
+//     log.debug('webcontent was crashed');
+// });
+
+// win.on('unresponsive', () => {
+//     // todo: prompt to restart
+//     log.debug('webcontent is unresponsive');
+// });
+
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// app.on('window-all-closed', () => {
+//     if (process.platform !== 'darwin') {
+//         app.quit()
+//     }
+// })
+
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    app.quit()
 })
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (win === null) {
         createWindow()
     }
