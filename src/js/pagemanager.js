ddd9d516c81b0ac7ebac273035ff314fa929044d
PaulWzlff <paulwenzlaff@gmail.com>
2019-08-21 20:44:01 +0200

    Getting nodes from repo working

--- src/js/pagemanager.js
+++ src/js/pagemanager.js
@@ -1,3 +1,5 @@
+//#region Initial PageManagement 
+
 /*Initial open StartScreen */
 var div = document.getElementById('mainDiv');
 let template = document.getElementById('StartScreenTemplate').content;
@@ -25,4 +27,62 @@ function openPage(PageName, Header) {
     var div = document.getElementById('mainDiv');
     div.appendChild(template);
     document.getElementById("header").innerHTML = Header;
+
+    switch (PageName) {
+        case ('LoginTemplate'):
+            LoadNodes();
+            break;
+        case ('StartScreenTemplate'):
+            break;
+        case ('AdressTemplate'):
+            break;
+        case ('BackupTemplate'):
+            break;
+        case ('BalanceTemplate'):
+            break;
+        case ('HomeTemplate'):
+            break;
+        case ('TranserTemplate'):
+            break;
+    }
 }
+//#endregion
+
+//#region Login Template Data
+function LoadNodes() {
+    var nodeList = document.getElementById('nodeList');
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = 'https://raw.githubusercontent.com/JOXW/securuscoin_nodes/master/securuscoin_nodes.json';
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function myFunction(arr) {
+        var nodeArray = new Array(arr.nodes.length);
+
+        for(i in arr.nodes){
+            var singleNode = new Array(4);
+            singleNode[0] = arr.nodes[i].name;
+            singleNode[1] = arr.nodes[i].url;
+            singleNode[2] = arr.nodes[i].port;
+            singleNode[3] = arr.nodes[i].ssl;
+            nodeArray[i] = singleNode;
+        }    
+
+        for(i in nodeArray){
+            var opt = document.createElement('option');
+            opt.value = nodeArray[i];
+            opt.innerHTML = nodeArray[i];
+            nodeList.appendChild(opt);
+        }
+    }   
+}
+//#endregion
